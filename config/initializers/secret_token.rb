2ed0249c27f174ef2f9e18fed7ff936839777722
etienne pierrot <etienne@leetchi.com>
2013-12-02 23:14:06 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeedMyFreebox::Application.config.secret_token = '9f914afb61e5bd36d2b0a22a5bec0a1ac3b2d7dacec6ca6ea1ff715b1f05233bcf1d3e3d46d4de0b432118e814c3c1a80438220cda6d0dd9a02f73e786fcec83'
