884b6b264f735bb9f960008926ff3fb03cfbd3d4
Etienne Pierrot <etienne.pierrot@leetchi.com>
2013-12-05 20:23:53 +0100

    maybe roolback this commit. it' doesn't work

--- lib/beta_series.rb
+++ lib/beta_series.rb
@@ -6,48 +6,79 @@ require_relative 'betaseries_connector'
 
 class SubtitleSelector
 
-  def 
-    
+  def fill_show_with_subtitles (show, teams, nbEpisodeMax, user_token)
+    betaseries_connector = BetaseriesConnector.new()
+    i = 0
+    if(!show["unseen"].nil?)
+      while i < 1 and !show["unseen"][i].nil?
+        puts i
+        puts show["unseen"][i]
+        episode = show["unseen"][i]
+
+        #puts "episode : " + episode
+
+
+        episode["subtitles"] = betaseries_connector.get_subtitles(user_token, episode["id"])
+
+        #keep only subtitle of recognized teams
+        episode["subtitles"].select!{|s| teams.any?{|t| s["file"].include?(t) }}
+        #episode["subtitles"].each{|s| puts s["file"]}
+        #puts episode["subtitles"]
+        i = i +1
+
+      end
+    end
+
+    return show
+
   end
 
 end
 
 code = ARGV[0]
 betaseries_connector = BetaseriesConnector.new()
+subtitle_selector = SubtitleSelector.new()
+
 user_token = betaseries_connector.get_user_token(code)
 shows = betaseries_connector.get_episodes(user_token)
 teams = ["LOL", "KILLERS", "IMMERSE", "DIMENSION", "WEB-DL", "2HD"]
+NB_EPISODE_MAX = 3
 
-shows.each do |s|
+#subtitle_selector.fill_show_with_subtitles(shows, teams, NB_EPISODE_MAX , user_token)
+#puts shows
+shows.each do |show|
 
-  episode = s["unseen"][0]
-  puts  episode["id"].to_s + " - " + s["title"] + " - " + episode["code"] + " _ " + episode["title"]
-  episode["subtitles"] = betaseries_connector.get_subtitles(user_token, episode["id"])
+  show = subtitle_selector.fill_show_with_subtitles(show, teams, NB_EPISODE_MAX , user_token)
+  i = 0
+  while i < 2 and !show["unseen"][i].nil?
 
-  #keep only subtitle of recognized teams
-  episode["subtitles"].select!{|s| teams.any?{|t| s["file"].include?(t) }}
-  episode["subtitles"].each{|s| puts s["file"]}
+    episode = !show["unseen"][i]
+    puts episode["subtitles"][0]["files"]
 
-  teams_who_have_subtitle = teams.select{|t| episode["subtitles"].any?{|s| s["file"].include?(t) }}
+    teams_who_have_subtitle = teams.select{|t| episode["subtitles"].any?{|s| s["file"].include?(t) }}
+    puts teams_who_have_subtitle
+    result = request_torrent( episode["code"] + " " + show["title"] + " 720p")
 
-  result = request_torrent( episode["code"] + " " + s["title"] + " 720p")
-
-  if result.nil?
-    puts "no torrent match found for #{s["title"]} - #{episode["code"]}"
-  else
-    result_parsed =  parse_list_episode(result)
-
-    episode["torrent"] = get_best_choice(result_parsed, teams_who_have_subtitle)
-
-    if episode["torrent"].nil?
-      puts "no match found for #{s["title"]} - #{episode["code"]}"
+    if result.nil?
+      puts "no torrent match found for #{show["title"]} - #{episode["code"]}"
     else
-      puts "match found for #{s["title"]} - #{episode["code"]}"
+      result_parsed =  parse_list_episode(result)
+
+      episode["torrent"] = get_best_choice(result_parsed, teams_who_have_subtitle)
+
+      if episode["torrent"].nil?
+        puts "no match found for #{show["title"]} - #{episode["code"]}"
+      else
+        puts "match found for #{show["title"]} - #{episode["code"]}"
+      end
     end
+    i = i + 1
   end
+
+
 end
 
-#shows.each{|e| puts  e["unseen"][0]["title"] }
+#show.each{|e| puts  e["unseen"][0]["title"] }
 
 
 
